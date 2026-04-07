55f7008f016e462d0636e8dfb38a2592f94bd7c6
timkosuge <timkosuge@gmail.com>
2026-04-07 11:37:20 -0500

    Update dashboard1.js

--- dashboard1.js
+++ dashboard1.js
@@ -1463,7 +1463,7 @@ function renderDesk(md,sd){
             if (!canvas) return;
             const dpr = window.devicePixelRatio || 1;
             const W = canvas.parentElement.clientWidth;
-            const H = 120;
+            const H = 200;
             canvas.width  = W * dpr; canvas.height = H * dpr;
             canvas.style.width = W + 'px'; canvas.style.height = H + 'px';
             const ctx = canvas.getContext('2d');
